6b4ab5076cd849b0f0a04c7a9cd9971bf87a97a4
monkcoders <abhisheksharma46002@gmail.com>
2023-03-11 16:58:01 +0530

    final update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const client = require("@mailchimp/mailchimp_marketing");
-const {config} = require('./config') 
+const config = require('./config') 
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
